8ae49e420c960dfaf6c28cd9480dce90037752c9
Mikhail <ramory.lorch@icloud.com>
2024-11-28 17:17:38 +0300

    cut num of lines shown on context

--- lua/custom/plugins/nvim-treesitter-context.lua
+++ lua/custom/plugins/nvim-treesitter-context.lua
@@ -2,6 +2,12 @@ return {
   {
     'nvim-treesitter/nvim-treesitter-context',
 
+    config = function()
+      require('treesitter-context').setup {
+        max_lines = 3,
+      }
+    end,
+
     vim.keymap.set('n', '<leader>jtc', function()
       require('treesitter-context').go_to_context(vim.v.count1)
     end, { desc = '[J]ump [T]o [C]ontext', silent = true }),
